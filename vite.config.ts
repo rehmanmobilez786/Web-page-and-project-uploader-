fb373774e4b1ef798f51e5941cb46a7446b4a425
rehmanmobilez786 <rehmanmobilez786@gmail.com>
2026-08-29 01:16:03 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+export default defineConfig(() => {
+  return {
+    base: './',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+  };
 });
